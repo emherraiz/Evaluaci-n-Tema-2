bb830a39cdc177455f84e492375fbef875f1ad0b
Enrique Marina <eherraiz@myuax.com>
2022-10-07 15:44:01 +0200

    sigo

--- alumnos.py
+++ alumnos.py
@@ -1,9 +1,19 @@
 class Alumno():
+    '''Esta es la clase alumno la cual nos vale para crear objetos de tipo alumno
+    '''
     def __init__(self, nombre, nota):
+        '''Constructor de la clase alumno
+
+        Args:
+            nombre (string): Nombre del alumno
+            nota (float): Nota del alumno
+        '''
         self.nombre = nombre
         self.nota = nota
 
     def aprobado(self):
+        '''Funcion que nos dice si el alumno esta aprobado o no
+        '''
         if self.nota >= 5:
             return True
         else:
