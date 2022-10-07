445e7c8475994d52a660ff31c3f0664b1617b1b7
Enrique Marina <eherraiz@myuax.com>
2022-10-07 16:29:15 +0200

    fds

--- helpers.py
+++ helpers.py
@@ -1,12 +1,27 @@
-import re
 import os
 import platform
 
 def limpiar_pantalla():
-    os.system('cls') if platform.system() == "Windows" else os.system('clear')
+    '''_summary_ = 'Funcion que limpia la pantalla'
+    '''
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
 
 
 def leer_texto(longitud_min=0, longitud_max=100, mensaje=None):
+    '''_summary_ = 'Funcion que lee un texto'
+
+    Args:
+        longitud_min (int, optional): Longitud minima. Defaults to 0.
+        longitud_max (int, optional): Longitud maxima. Defaults to 100.
+        mensaje (string, optional): Lee el texto introducido. Defaults to None.
+
+    Returns:
+        string: En caso de no introducir texto en la función lo introduces por medio del teclado
+    '''
+
     print(mensaje) if mensaje else None
     while True:
         texto = input("> ")
